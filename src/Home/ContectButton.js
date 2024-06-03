90ed255e7e04161c81db7f49e91f8a20eaa1bc09
Fehzan123 <mohdfehzan1998@gmail.com>
2024-06-03 21:42:57 +0530

    final tuch

--- src/Home/ContectButton.js
+++ src/Home/ContectButton.js
@@ -14,7 +14,7 @@ const ContactButton = () => {
     <div className='HomeContainer'>
       <div className='Home'>
         <div>
-          <h3>AWARD WINNING</h3>
+          <h3 className='Award'>AWARD WINNING</h3>
           <h1>DIGITAL MARKETING AGENCY</h1>
           <p className='HomePara'>
             Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque vitae ante at elit fringilla ac at purus, Morbi sed lacus nec risus finibus feugiat et fermentum
